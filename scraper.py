3e6ca63ded0a92bac30f0a435c2207d5bb7db7f6
Ian Watt <ianwatt@gmail.com>
2016-10-11 13:06:40 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -9,7 +9,7 @@ import scraperwiki
 # # Read in a page
 
 def scrape_URL (url_no):
-    scrape_add = "http://www.grampiancaredata.gov.uk/home?catid=" & str (url_no)
+    scrape_add = "http://www.grampiancaredata.gov.uk/home?catid=" + str(url_no)
     html = scraperwiki.scrape(scrap_add)
     return html
 
